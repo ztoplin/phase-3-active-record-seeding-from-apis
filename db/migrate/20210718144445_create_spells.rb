46d542d24cd76092f0d303e17e29863ea31bc311
Aaron <ztoplin@yahoo.com>
2022-11-29 11:19:57 -0500

    commit tree 7a84d82b3d525ed0badaee7c41580f60d926f9a1

--- db/migrate/20210718144445_create_spells.rb
+++ db/migrate/20210718144445_create_spells.rb
@@ -1,9 +1,13 @@
 class CreateSpells < ActiveRecord::Migration[6.1]
+
   def change
+
     create_table :spells do |t|
       t.string :name
       t.integer :level
       t.string :description
     end
+    
   end
+
 end
